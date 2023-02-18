676b91087e370a76b578cbd7ee6d53154919aeb9
Aqirlone1 <aqirlone109@gmail.com>
2023-02-18 21:44:57 +0530

    made an component for footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import FirstComponent from "./components/aima";
 
 function App() {
   return (
     <div className="App">
       <h1> Welcome To Kashur Scientist.........</h1>
+      <h1> This is testing ....</h1>
+      <h1> hello testing again </h1>
+      <FirstComponent />
     </div>
   );
 }
