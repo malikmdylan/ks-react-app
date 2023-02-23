04d379f7cc97a919a02790fa1423ba48cbd8bf72
Malik Mubarak Dylan <malikcquantum@gmail.com>
2023-02-23 23:10:04 +0530

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,13 +4,7 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-left">
-        <a className="company" href=" "> Kashur Scientist </a>{" "}
-      </div>{" "}
-      <div className="navbar-right">
-        <a href=" "> Courses </a> <a href="m"> About </a>{" "}
-        <a href=" "> Contact </a> <a href=" "> Our Team </a>{" "}
-      </div>{" "}
+      //for route
     </nav>
   );
 }
